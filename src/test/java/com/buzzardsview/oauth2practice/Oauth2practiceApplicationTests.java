48459036415540dee74c6bc10ea76b261751bca9
cbuzzard <corwin1998@gmail.com>
2020-08-22 21:26:15 -0400

    init

---
+++ src/test/java/com/buzzardsview/oauth2practice/Oauth2practiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buzzardsview.oauth2practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2practiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
