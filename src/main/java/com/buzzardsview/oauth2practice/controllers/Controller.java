fab8554f3bd64570ddb7d61ed7e5c76797858b4e
cbuzzard <corwin1998@gmail.com>
2020-08-23 20:00:45 -0400

    mapping for user

--- src/main/java/com/buzzardsview/oauth2practice/controllers/Controller.java
+++ src/main/java/com/buzzardsview/oauth2practice/controllers/Controller.java
@@ -1,7 +1,10 @@
 package com.buzzardsview.oauth2practice.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 public class Controller {
@@ -14,6 +17,11 @@ public class Controller {
     @GetMapping("/restricted")
     public String restricted() {
         return "to see this you must be logged in";
+    }
+
+    @RequestMapping(value = "/user")
+    public Principal user(Principal principal) {
+        return principal;
     }
 
 }
