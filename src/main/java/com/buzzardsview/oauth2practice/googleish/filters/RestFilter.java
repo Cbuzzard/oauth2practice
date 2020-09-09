93d5cbca86999cd7d24cfc2e8861818ae8df36d1
cbuzzard <corwin1998@gmail.com>
2020-09-08 23:37:52 -0400

    authentication filters

---
+++ src/main/java/com/buzzardsview/oauth2practice/googleish/filters/RestFilter.java
@@ -0,0 +1,47 @@
+package com.buzzardsview.oauth2practice.googleish.filters;
+
+import java.io.IOException;
+import java.util.Optional;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import static com.buzzardsview.oauth2practice.googleish.AppTokenProvider.addAuthentication;
+import static com.buzzardsview.oauth2practice.googleish.AppTokenProvider.getUserFromToken;
+
+@Component
+public class RestFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        Optional<String> userFromToken = getUserFromToken(request);
+
+        if (!userFromToken.isPresent()) {
+            response.sendError(HttpStatus.UNAUTHORIZED.value());
+            return;
+        }
+        request.setAttribute("userId", userFromToken.get());
+        addAuthentication(response, userFromToken.get());
+        filterChain.doFilter(request, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
